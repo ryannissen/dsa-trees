cd3f00fca3c27b953c099ab840038e7517d5c63a
Ryan Nissen <ryankn852@gmail.com>
2022-04-13 13:26:29 -0500

    Completed sumValues and countEvens

--- tree.js
+++ tree.js
@@ -16,12 +16,47 @@ class Tree {
 
   sumValues() {
     
+    let toVisitStack = [this.root];
+    let sum = 0;
+
+    if (toVisitStack[0] === null || toVisitStack[0].children.length === 0 ) return 0;
+
+    while (toVisitStack.length) {
+
+      let current = toVisitStack.pop();
+
+      sum += current.val;
+
+      for (let child of current.children) {
+        toVisitStack.push(child);
+      }
+
+    }
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    let toVisitStack = [this.root];
+    let nodesWithEvens = 0;
 
+    if (toVisitStack[0] === null || toVisitStack[0].children.length === 0 ) return 0;
+
+    while (toVisitStack.length) {
+
+      let current = toVisitStack.pop();
+
+      if (current.val % 2 === 0){
+        nodesWithEvens++;
+      }
+
+      for (let child of current.children) {
+        toVisitStack.push(child);
+      }
+
+    }
+    return nodesWithEvens;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
